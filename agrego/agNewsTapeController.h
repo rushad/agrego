6cf98c7f79e88b3d67c2a6c279027dce977ac7e3
paullasarev <paullasarev@gmail.com>
2014-03-18 08:04:05 +0400

    add storyboard and tabbed view controller

---
+++ agrego/agNewsTapeController.h
@@ -0,0 +1,13 @@
+//
+//  agNewsTapeController.h
+//  agrego
+//
+//  Created by  Pavel Lasarev on 16/03/14.
+//  Copyright (c) 2014 Rushad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface agNewsTapeController : UITableViewController
+
+@end
